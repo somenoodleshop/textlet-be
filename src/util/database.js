77202b9f2a4372353e8aacc71673932990feae02
arandombit <a.random.luis@proton.me>
2026-01-23 22:59:49 -0500

    configure database connection with kysely

--- src/util/database.js
+++ src/util/database.js
@@ -1,2 +1,19 @@
 import { Kysely, PostgresDialect } from 'kysely'
-import pg from 'pg'
+import { Pool } from 'pg'
+
+const { DB_HOST = 'localhost', DB_PORT = 5432, DB_NAME = 'textlet', DB_USER = 'postgres', DB_PASSWORD = '' } = process.env
+
+const database = new Kysely({
+  dialect: new PostgresDialect({
+    pool: new Pool({
+      host: DB_HOST,
+      port: DB_PORT,
+      database: DB_NAME,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      max: 10 // Maximum number of clients in the pool
+    })
+  })
+})
+
+export default database
